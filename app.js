34c18d58e9b4e61bfff4b3e2c2d6adc9361cc7cb
Marcus <marcusicaromc@gmail.com>
2023-08-20 22:22:41 -0300

    fix Multer

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('dotenv').config();
 const MongoDBKey = process.env.MONGODB_KEY;
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var routes = require('./routes/routes');
 
 var app = express();
 
@@ -34,8 +33,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
