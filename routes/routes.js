77387ba945e60f9f6d77297d5e5d0d04654bbe5c
Marcus <marcusicaromc@gmail.com>
2023-08-22 07:27:10 -0300

    uploading images to mongodb

--- routes/routes.js
+++ routes/routes.js
@@ -4,13 +4,25 @@ const router = express.Router();
 const item_controller = require('../controllers/itemsController');
 const category_controller = require('../controllers/categoryController');
 const multer = require('multer');
+const { GridFsStorage } = require('multer-gridfs-storage');
+require('dotenv').config();
+const MongoDBKey = process.env.MONGODB_KEY;
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'public/');
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now() + '.png');
+const url = `mongodb+srv://admin:${MongoDBKey}@cluster0.lnrds0m.mongodb.net/inventory_application?retryWrites=true&w=majority`;
+
+const storage = new GridFsStorage({
+  url,
+  file: (req, file) => {
+    //If it is an image, save to photos bucket
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+      return {
+        bucketName: 'photos',
+        filename: `${Date.now()}_${file.originalname}`,
+      };
+    } else {
+      //Otherwise save to default bucket
+      return `${Date.now()}_${file.originalname}`;
+    }
   },
 });
 
